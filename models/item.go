30bb7cea4ccc5976e073780b0cf73c8d3e237213
jianggushi <rogerjiangx@outlook.com>
2019-10-18 08:00:26 +0800

    update models item

--- models/item.go
+++ models/item.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"encoding/json"
+
+	"github.com/jinzhu/gorm"
+)
 
 // Item 历史热榜信息
 type Item struct {
@@ -9,9 +13,25 @@ type Item struct {
 	Description string
 	Thumbnail   string
 	URL         string `gorm:"column:url"`
-	MD5         string `gorm:"column:md5"`
+	MD5         string `gorm:"column:md5;unique_index"`
 	Extra       string
 	NodeID      int64 `gorm:"column:node_id"` // 所属 node id
+}
+
+func SaveItems(items []*Item) error {
+	for _, item := range items {
+		if db.Where("md5 = ?", item.MD5).First(item).RecordNotFound() {
+			// create new record
+		} else {
+			// update record
+		}
+	}
+	// save items to lastitem
+	data, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func init() {
