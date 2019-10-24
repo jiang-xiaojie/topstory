6cf433123200bf7f2894487196674e697aa4c336
jianggushi <rogerjiangx@outlook.com>
2019-10-24 19:01:22 +0800

    update miniprogram

--- public/miniprogram/pages/index/index.js
+++ public/miniprogram/pages/index/index.js
@@ -4,51 +4,22 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    lastItems: [
+      {
+        'title': '妻子的浪漫旅行 杜江霍思燕',
+        'description': ''
+      }
+    ]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+  onLoad() {
+    wx.request({
+      url: 'http://127.0.0.1:8080/nodes/1/lastitem',
+      success: res => {
+        let items = res.data.data.items
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          lastItems: items
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
     })
   }
 })
